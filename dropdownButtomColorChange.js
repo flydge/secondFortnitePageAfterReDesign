e9895529e5c96eccecbfa94373e56f00d9030bc9
thunderFlydge <thunderFlydge@yandex.ru>
2018-09-15 14:37:01 +0300

    added: issues fix and adaptive css

--- dropdownButtomColorChange.js
+++ dropdownButtomColorChange.js
@@ -1,13 +1,47 @@
+/*
 
-const dropdown = document.getElementsByClassName('topContent-container-navigation-topRow-dropdowns-dropdown');
-const button = document.getElementsByClassName('topContent-container-navigation-topRow-dropdowns-dropdown_button');
-for (let i = 0; i < dropdown.length ; i++) {
-    dropdown[i].addEventListener('mouseover', ()=>{
-        button[i].style.color='black';
+const dropdowns  = document.getElementsByClassName('topContent-container-navigation-topRow-dropdowns');
 
-    });
-    dropdown[i].addEventListener('mouseout', ()=>{
-        button[i].style.color='white';
+for (let i = 0; i < dropdowns[0].childElementCount ; i++) {
 
-    })
+	const dropdown = dropdowns[0].children[i];
+	dropdown.addEventListener('mouseover', ()=>{
+		dropdown.firstElementChild.style.color='black';
+		dropdown.firstElementChild.lastElementChild.style.color='black';
+		dropdown.firstElementChild.style.zIndex='51';
+		dropdown.lastElementChild.style.zIndex='50';
+	});
+	dropdown.addEventListener('mouseout', ()=>{
+		dropdown.firstElementChild.style.color='white';
+		dropdown.firstElementChild.lastElementChild.style.color='white';
+		dropdown.firstElementChild.style.zIndex='21';
+		dropdown.lastElementChild.style.zIndex='20';
+	});
+}
+*/
+
+
+
+
+var dropdowns = document.getElementsByClassName('topContent-container-navigation-topRow-dropdowns');
+
+var _loop = function _loop(i) {
+
+	var dropdown = dropdowns[0].children[i];
+	dropdown.addEventListener('mouseover', function () {
+		dropdown.firstElementChild.style.color = 'black';
+		dropdown.firstElementChild.lastElementChild.style.color = 'black';
+		dropdown.firstElementChild.style.zIndex = '51';
+		dropdown.lastElementChild.style.zIndex = '50';
+	});
+	dropdown.addEventListener('mouseout', function () {
+		dropdown.firstElementChild.style.color = 'white';
+		dropdown.firstElementChild.lastElementChild.style.color = 'white';
+		dropdown.firstElementChild.style.zIndex = '21';
+		dropdown.lastElementChild.style.zIndex = '20';
+	});
+};
+
+for (var i = 0; i < dropdowns[0].childElementCount; i++) {
+	_loop(i);
 }
